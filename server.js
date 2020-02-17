f3ea3b297abbe3c2cd9cd6ed57ab0e8cb9633451
mjahere023@gmail.com <mjahere023@gmail.com>
2020-02-16 18:29:30 -0700

    added git ignore and minore changes to server.

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 const express = require("express");
 const logger = require("morgan");
 const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv")
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -20,12 +19,15 @@ app.use(express.static("public"));
 
 // connection to mongoDB 
 mongoose.connect(process.env.MONGODB_URI, {
-     useNewUrlParser: true,
-     useUnifiedTopology: true
-    }).
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).
 
+
+    app.use(require("./routes/view.js"));
+app.use(require("./routes/api.js"));
 
 // Server Listen
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
-  });
+});
